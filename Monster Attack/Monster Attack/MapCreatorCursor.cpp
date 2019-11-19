f729f025bbb418c64db4a2248cdea5eaca81716b
MatthewN <northmatt1@gmail.com>
2019-11-18 21:26:05 -0500

    Map Creator (WIP)
    -fill function complete (laggy tho)

--- Monster Attack/Monster Attack/MapCreatorCursor.cpp
+++ Monster Attack/Monster Attack/MapCreatorCursor.cpp
@@ -40,74 +40,89 @@ void MapCreatorCursor::Update() {
 	else if (pos.y > maxPos.y)
 		pos.y = maxPos.y;
 
-	if (Input::GetKeyDown(VK_NUMPAD4))
-		colorValInd = !colorValInd;
+	//basic drawing
+	{
+		if (Input::GetKeyDown(VK_NUMPAD4))
+			colorValInd = !colorValInd;
 
-	if (Input::GetKeyDown(VK_SPACE))
-		isDrawing = !isDrawing;
+		if (Input::GetKeyDown(VK_SPACE))
+			isDrawing = !isDrawing;
 
-	if (Input::GetKeyDown(VK_NUMPAD7))
-		colorValues[colorValInd][0] = !colorValues[colorValInd][0];
-	else if (Input::GetKeyDown(VK_NUMPAD8))
-		colorValues[colorValInd][1] = !colorValues[colorValInd][1];
-	else if (Input::GetKeyDown(VK_NUMPAD9))
-		colorValues[colorValInd][2] = !colorValues[colorValInd][2];
-	else if (Input::GetKeyDown(VK_NUMPAD5))
-		colorValues[colorValInd][3] = !colorValues[colorValInd][3];
+		if (Input::GetKeyDown(VK_NUMPAD7))
+			colorValues[colorValInd][0] = !colorValues[colorValInd][0];
+		else if (Input::GetKeyDown(VK_NUMPAD8))
+			colorValues[colorValInd][1] = !colorValues[colorValInd][1];
+		else if (Input::GetKeyDown(VK_NUMPAD9))
+			colorValues[colorValInd][2] = !colorValues[colorValInd][2];
+		else if (Input::GetKeyDown(VK_NUMPAD5))
+			colorValues[colorValInd][3] = !colorValues[colorValInd][3];
 
-	color = 1;
+		color = 1;
 
-	if (colorValues[0][0])
-		color = color | FOREGROUND_RED;
-	if (colorValues[0][1])
-		color = color | FOREGROUND_GREEN;
-	if (colorValues[0][2])
-		color = color | FOREGROUND_BLUE;
-	if (colorValues[0][3])
-		color = color | FOREGROUND_INTENSITY;
+		if (colorValues[0][0])
+			color = color | FOREGROUND_RED;
+		if (colorValues[0][1])
+			color = color | FOREGROUND_GREEN;
+		if (colorValues[0][2])
+			color = color | FOREGROUND_BLUE;
+		if (colorValues[0][3])
+			color = color | FOREGROUND_INTENSITY;
 
-	if (colorValues[1][0])
-		color = color | BACKGROUND_RED;
-	if (colorValues[1][1])
-		color = color | BACKGROUND_GREEN;
-	if (colorValues[1][2])
-		color = color | BACKGROUND_BLUE;
-	if (colorValues[1][3])
-		color = color | BACKGROUND_INTENSITY;
+		if (colorValues[1][0])
+			color = color | BACKGROUND_RED;
+		if (colorValues[1][1])
+			color = color | BACKGROUND_GREEN;
+		if (colorValues[1][2])
+			color = color | BACKGROUND_BLUE;
+		if (colorValues[1][3])
+			color = color | BACKGROUND_INTENSITY;
 
-	if (isDrawing)
-		for (size_t i = 0; i < 2; i++) {
-			Game::shared_instance().GetCurrentScene()->writeScreen[round(pos.x - 1) * 2 + i + round(pos.y - 1) * maxPos.x * 2] = ' ';
-			Game::shared_instance().GetCurrentScene()->colorScreen[round(pos.x - 1) * 2 + i + round(pos.y - 1) * maxPos.x * 2] = color;
-		}
+		if (isDrawing)
+			for (size_t i = 0; i < 2; i++) {
+				Game::shared_instance().GetCurrentScene()->writeScreen[round(pos.x - 1) * 2 + i + round(pos.y - 1) * maxPos.x * 2] = ' ';
+				Game::shared_instance().GetCurrentScene()->colorScreen[round(pos.x - 1) * 2 + i + round(pos.y - 1) * maxPos.x * 2] = color;
+			}
+	}
 
-	if (Input::GetKeyDown('p')) {
+	//fill function
+	if (Input::GetKeyDown('P')) {
+		size_t currSize{ 1 };
 		size_t preSize{ 0 };
-		vector<size_t> posToIgnore;
 		vector<vector<int>> posList;
 		vector<vector<int>> movePoints{ {0, 1}, {2, 0}, {0, -1}, {-2, 0} };
-		char currentChar = Game::shared_instance().GetCurrentScene()->writeScreen[round(pos.x - 1) * 2 + round(pos.y - 1) * maxPos.x * 2];
-		int currentColor = Game::shared_instance().GetCurrentScene()->colorScreen[round(pos.x - 1) * 2 + round(pos.y - 1) * maxPos.x * 2];
-		posList.push_back({ static_cast<int>(round(pos.x - 1) * 2), static_cast<int>(round(pos.y - 1)) });
+		posList.push_back({ static_cast<int>(round(pos.x - 1) * 2), static_cast<int>(round(pos.y - 1)), 0 });
+		char currentChar = Game::shared_instance().GetCurrentScene()->writeScreen[posList[0][0] + posList[0][1] * maxPos.x * 2];
+		int currentColor = Game::shared_instance().GetCurrentScene()->colorScreen[posList[0][0] + posList[0][1] * maxPos.x * 2];
+		/*
+		3012031
+		1000101
+		1200110
+		0011101
 
-		while (posList.size() != preSize) {
-			preSize = posList.size();
-			for (size_t i = 0; i > posList.size(); i++) {
-				bool shouldIgnore{ false };
-				for (size_t checkIgnore : posToIgnore)
-					if (i == checkIgnore) {
-						shouldIgnore = true;
-						break;
-					}
+		ran starting on a "0"
 
-				if (shouldIgnore)
+		0100000
+		0111010
+		0011001
+		1100010
+		*/
+		while (currSize != preSize) {
+			preSize = currSize;
+			currSize = posList.size();
+			for (size_t i = 0; i < posList.size(); i++) {
+				if (posList[i][2] == 1)
 					continue;
 
 				for (vector<int> moveVec : movePoints) {
 					bool exists{ false };
-					vector<int> toAddVec{ posList[i][0] + moveVec[0], posList[i][1] + moveVec[1] };
+					vector<int> toAddVec{ posList[i][0] + moveVec[0], posList[i][1] + moveVec[1], 0 };
 
 					//add code to detect if point is on different curr Char/Color or off map
+					if (toAddVec[0] < 0 || toAddVec[0] >= maxPos.x * 2 || toAddVec[1] < 0 || toAddVec[1] >= maxPos.y)
+						continue;
+
+					if (Game::shared_instance().GetCurrentScene()->writeScreen[toAddVec[0] + toAddVec[1] * maxPos.x * 2] != currentChar || Game::shared_instance().GetCurrentScene()->colorScreen[toAddVec[0] + toAddVec[1] * maxPos.x * 2] != currentColor)
+						continue;
 
 					for (vector<int> checkCol : posList)
 						if (toAddVec[0] == checkCol[0] && toAddVec[1] == checkCol[1]) {
@@ -118,9 +133,16 @@ void MapCreatorCursor::Update() {
 					if (exists)
 						continue;
 
-					posToIgnore.push_back(i);
+					posList[i][2] = 1;
 					posList.push_back(toAddVec);
 				}
+			}
+		}
+
+		for (vector<int> currVec : posList) {
+			for (size_t index = 0; index < 2; index++) {
+				Game::shared_instance().GetCurrentScene()->writeScreen[currVec[0] + index + currVec[1] * maxPos.x * 2] = currentChar;
+				Game::shared_instance().GetCurrentScene()->colorScreen[currVec[0] + index + currVec[1] * maxPos.x * 2] = color;
 			}
 		}
 	}
