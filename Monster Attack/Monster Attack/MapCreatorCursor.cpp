ae79dc395ccce9f920788f175f379db42faf321b
MatthewN <northmatt1@gmail.com>
2019-11-19 00:22:52 -0500

    Map Creator (WIP)
    -Fill function revamp. Now significantly faster

--- Monster Attack/Monster Attack/MapCreatorCursor.cpp
+++ Monster Attack/Monster Attack/MapCreatorCursor.cpp
@@ -86,65 +86,12 @@ void MapCreatorCursor::Update() {
 
 	//fill function
 	if (Input::GetKeyDown('P')) {
-		size_t currSize{ 1 };
-		size_t preSize{ 0 };
-		vector<vector<int>> posList;
-		vector<vector<int>> movePoints{ {0, 1}, {2, 0}, {0, -1}, {-2, 0} };
-		posList.push_back({ static_cast<int>(round(pos.x - 1) * 2), static_cast<int>(round(pos.y - 1)), 0 });
-		char currentChar = Game::shared_instance().GetCurrentScene()->writeScreen[posList[0][0] + posList[0][1] * maxPos.x * 2];
-		int currentColor = Game::shared_instance().GetCurrentScene()->colorScreen[posList[0][0] + posList[0][1] * maxPos.x * 2];
-		/*
-		3012031
-		1000101
-		1200110
-		0011101
+		vector<int> currentPos{ static_cast<int>(round(pos.x - 1) * 2), static_cast<int>(round(pos.y - 1)), 0 };
+		char currentChar = Game::shared_instance().GetCurrentScene()->writeScreen[currentPos[0] + currentPos[1] * maxPos.x * 2];
+		int currentColor = Game::shared_instance().GetCurrentScene()->colorScreen[currentPos[0] + currentPos[1] * maxPos.x * 2];
 
-		ran starting on a "0"
-
-		0100000
-		0111010
-		0011001
-		1100010
-		*/
-		while (currSize != preSize) {
-			preSize = currSize;
-			currSize = posList.size();
-			for (size_t i = 0; i < posList.size(); i++) {
-				if (posList[i][2] == 1)
-					continue;
-
-				for (vector<int> moveVec : movePoints) {
-					bool exists{ false };
-					vector<int> toAddVec{ posList[i][0] + moveVec[0], posList[i][1] + moveVec[1], 0 };
-
-					//add code to detect if point is on different curr Char/Color or off map
-					if (toAddVec[0] < 0 || toAddVec[0] >= maxPos.x * 2 || toAddVec[1] < 0 || toAddVec[1] >= maxPos.y)
-						continue;
-
-					if (Game::shared_instance().GetCurrentScene()->writeScreen[toAddVec[0] + toAddVec[1] * maxPos.x * 2] != currentChar || Game::shared_instance().GetCurrentScene()->colorScreen[toAddVec[0] + toAddVec[1] * maxPos.x * 2] != currentColor)
-						continue;
-
-					for (vector<int> checkCol : posList)
-						if (toAddVec[0] == checkCol[0] && toAddVec[1] == checkCol[1]) {
-							exists = true;
-							break;
-						}
-
-					if (exists)
-						continue;
-
-					posList[i][2] = 1;
-					posList.push_back(toAddVec);
-				}
-			}
-		}
-
-		for (vector<int> currVec : posList) {
-			for (size_t index = 0; index < 2; index++) {
-				Game::shared_instance().GetCurrentScene()->writeScreen[currVec[0] + index + currVec[1] * maxPos.x * 2] = currentChar;
-				Game::shared_instance().GetCurrentScene()->colorScreen[currVec[0] + index + currVec[1] * maxPos.x * 2] = color;
-			}
-		}
+		if (currentColor != color)
+			FillLoop(currentPos, -1, currentChar, currentColor);
 	}
 
 	/*for (size_t y = 0; y < 2; y++) {
@@ -155,4 +102,57 @@ void MapCreatorCursor::Update() {
 
 	Game::shared_instance().buffer.SetCamPos({ static_cast<int>(round(pos.x)), static_cast<int>(round(pos.y)) });
 	Game::shared_instance().buffer.WriteBuffer(image, round(pos.x), pos.y, color);
+}
+
+void MapCreatorCursor::FillLoop(vector<int> pos, int direction, char oldChar, int oldColor) {
+	vector<vector<int>> movePoints{ {0, 1}, {2, 0}, {0, -1}, {-2, 0} };
+
+	struct SnapShotStruct {
+		vector<int> pos;
+		int direction;
+		char oldChar;
+		int oldColor;
+	};
+
+	stack<SnapShotStruct> snapshotStack;
+	// (Fourth rule)
+	SnapShotStruct currentSnapshot;
+	currentSnapshot.pos = pos;
+	currentSnapshot.direction = direction;
+	currentSnapshot.oldChar = oldChar;
+	currentSnapshot.oldChar = oldColor;
+	snapshotStack.push(currentSnapshot);
+
+	while (!snapshotStack.empty()) {
+		currentSnapshot = snapshotStack.top();
+		snapshotStack.pop();
+
+		for (size_t i = 0; i < movePoints.size(); i++)
+			if (i != direction) {
+				vector<int> newVec{ currentSnapshot.pos[0] + movePoints[i][0], currentSnapshot.pos[1] + movePoints[i][1] };
+
+				//add code to detect if point is on different curr Char/Color or off map
+				if (newVec[0] < 0 || newVec[0] >= maxPos.x * 2 || newVec[1] < 0 || newVec[1] >= maxPos.y)
+					continue;
+
+				if (Game::shared_instance().GetCurrentScene()->writeScreen[newVec[0] + newVec[1] * maxPos.x * 2] != oldChar || Game::shared_instance().GetCurrentScene()->colorScreen[newVec[0] + newVec[1] * maxPos.x * 2] != oldColor)
+					continue;
+
+				for (size_t index = 0; index < 2; index++) {
+					Game::shared_instance().GetCurrentScene()->writeScreen[newVec[0] + index + newVec[1] * maxPos.x * 2] = oldChar;
+					Game::shared_instance().GetCurrentScene()->colorScreen[newVec[0] + index + newVec[1] * maxPos.x * 2] = color;
+				}
+
+				snapshotStack.push(currentSnapshot);
+
+				SnapShotStruct newSnapshot;
+				newSnapshot.pos = newVec;
+				newSnapshot.direction = i;
+				newSnapshot.oldChar = oldChar;
+				newSnapshot.oldChar = oldColor;
+
+				snapshotStack.push(newSnapshot);
+				continue;
+			}
+	}
 }
--- Monster Attack/Monster Attack/MapCreatorCursor.h
+++ Monster Attack/Monster Attack/MapCreatorCursor.h
@@ -1,15 +1,18 @@
 #pragma once
 
+#include <stack>
 #include "Scene.h"
 #include "Game.h"
 
 using std::round;
+using std::stack;
 
 class MapCreatorCursor : public Entity {
 public:
 	MapCreatorCursor(string inImage, string inName, double inPosX, double inPosY, int col);
 
 	void Update();
+	void FillLoop(vector<int> pos, int direction, char oldChar, int oldColor);
 private:
 	Vec2 vel;
 	double speed{ 8 };
